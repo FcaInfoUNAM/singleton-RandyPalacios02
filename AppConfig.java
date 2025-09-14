b1d1bb35560820fdf0974ae3e1f8be124aca5a08
RandyPalacios02 <randypro20@comunidad.unam.mx>
2025-09-14 00:25:50 +0000

    Practica  Singleton

--- AppConfig.java
+++ AppConfig.java
@@ -2,12 +2,19 @@ public class AppConfig {
     private String theme;
     private String language;
 
-    public AppConfig() {
+    private static final AppConfig instance = new AppConfig();
+
+    private AppConfig() {
         // Load default settings
         this.theme = "Light";
         this.language = "EN";
         System.out.println("New AppConfig instance created!");
     }
+
+    public static AppConfig getInstance() {
+    return instance;
+    }
+
 
     public String getTheme() { return theme; }
     public void setTheme(String theme) { this.theme = theme; }
